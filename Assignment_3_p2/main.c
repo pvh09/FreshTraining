eeb63a6374fcc7d686e7f08c1e90682e32dbf505
pvh09 <huyhoa09012001@gmail.com>
2023-08-31 15:56:19 +0700

    update assignment 3_P2

--- Assignment_3_p2/main.c
+++ Assignment_3_p2/main.c
@@ -1,14 +1,15 @@
 #include "caesar.h"
 
-void menu(char* file_name){
-    int choice, shift;
-    
-    shift = input_shift(shift);
+void menu(char *file_name, char *output_file_name, int shift)
+{
+    int choice;
     system("clear");
-    do {
+    do
+    {
         printf("\n\n=========================");
-        printf("\n>> %s (open)", file_name);  
-        printf("\n>> %d (shift)", shift); 
+        printf("\n>> Input file:  %s", file_name);
+        printf("\n>> Output file opened: %s", output_file_name);
+        printf("\n>> %d (shift)", shift);
         printf("\n  --------OPTION-------\n");
         printf("      1. Encrypt a string\n");
         printf("      2. Decrypt a string\n");
@@ -18,37 +19,43 @@ void menu(char* file_name){
         printf("Enter your choice: ");
         choice = validate_num_input();
 
-        switch (choice) {
-            case ENCRYPT:
-                printf("\nEncrypted content: \n");
-                encrypt(file_name, shift, 1);
-                break;
-            case DECRYPT:
-                printf("\nDecrypted content: \n");
-                decrypt(file_name, shift);
-                break;
-            case CHANGE_SHIFT:
-                printf("New shift - ");
-                menu(file_name);
-                break;
-            case CHANGE_OUTPUT_FILE:
-                change_new_output_file(file_name, shift);
-                break;
-            case EXIT_PROGRAM:
-                printf(">> Exiting program.\n");
-                exit(1);
-            default:
-                printf("Invalid choice. Please enter a valid option.\n");
+        switch (choice)
+        {
+        case ENCRYPT:
+            printf("\nEncrypted content: \n");
+            encrypt(file_name, shift, 1, output_file_name);
+            break;
+        case DECRYPT:
+            printf("\nDecrypted content: \n");
+            decrypt(file_name, shift, 0, output_file_name);
+            break;
+        case CHANGE_SHIFT:
+            printf("New shift - ");
+            shift = input_shift(shift);
+            menu(file_name, output_file_name, shift);
+            break;
+        case CHANGE_OUTPUT_FILE:
+            change_output_file(output_file_name);
+            menu(file_name, output_file_name, shift);
+            break;
+        case EXIT_PROGRAM:
+            printf(">> Exiting program.\n");
+            exit(1);
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
         }
     } while (choice != 4);
 }
 
-int main(){
+int main()
+{
 
     char file_name[MAX_FILE_NAME];
     char output_file_name[MAX_FILE_NAME];
     input_file(file_name);
-    menu(file_name);
-    
+    change_output_file(output_file_name);
+    int shift = input_shift(shift);
+    menu(file_name, output_file_name, shift);
+
     return 0;
 }
--- Assignment_3_p2/src/caesar.c
+++ Assignment_3_p2/src/caesar.c
@@ -28,66 +28,143 @@ int input_shift(int shift)
     return shift;
 }
 
-static void is_alpha_num(char* file_name){
-// Check if the input contains only numbers and alphabets
-    int valid_input = 1;
-    for (int i = 0; file_name[i] != '\0'; i++)
-    {
-        if (!isalnum(file_name[i]) && file_name[i] != '.')
-        {
-            valid_input = 0;
-            break;
+// Check if the file name contains any invalid characters
+static bool is_invalid_file_name(const char *file_name)
+{
+    const char *invalid_chars = "\\/:*?\"<>|";
+    for (int i = 0; i < strlen(invalid_chars); i++) {
+        if (strchr(file_name, invalid_chars[i]) != NULL) {
+            return false;
         }
     }
-    if (!valid_input)
-    {
-        printf(">> Invalid input. Please use only numbers and alphabets.\n");
-    }
-    
-    if(strlen(file_name) > 10){
-        printf(">> File name too long. Please enter a shorter file name.\n");
-    }    
+    return true;
 }
 
-static bool check_file_exist(char* file_name){
+//check file output is existed in folder
+static bool is_exist_out_file(char* file_name)
+{
+    char ch;
     FILE *fp;
     fp = fopen(file_name, "r");
     if (fp == NULL)
     {
+        printf(">> Create a output file success!!\n");
+        return true;
+    }
+    else 
+    {
+        printf("Do you want to append output file (Y) or (N)? \n");
+        printf("You choose: ");
+        ch = getchar();
+        if(ch == 'Y')
+        {
+            printf(">> Append output file success!!\n");
+            return true;
+        }
+    }
+    printf(">> This file is existed in folder. Please enter other output file!!\n");
+    if(fp!= NULL) 
+    {
+        fclose(fp);
+    }
+    return false;
+}
+
+//check length of output name and check name of ouput have no " \/:*?\"<>| "
+static bool is_over_range(char* file_name){
+    bool valid_input;
+    bool check_exist_file = false;
+    if(strlen(file_name) > MAX_FILE_NAME){
+        printf(">> File name too long. Please enter a shorter file name.\n");
+        return check_exist_file;
+    } 
+
+    valid_input = is_invalid_file_name(file_name);
+    if (!valid_input)
+    {
+        printf(">> Invalid input. Do not enter: \\/:*?\"<>| \n");
+        return check_exist_file;
+    }
+    else
+    {
+        check_exist_file = is_exist_out_file(file_name);
+    }
+    return check_exist_file;
+}
+
+//check if enter "/////////" => false
+static bool check_digit(char* file_name){
+    int i = 0;
+    for(i; i < strlen(file_name); i++){
+        if(file_name[i] != '\\' && file_name[i] != '/'){
+            return true;
+        }
+    }
+    return false;
+}
+
+//check file input is existed in folder
+static bool is_exist_input_file(char* file_name)
+{
+    FILE *fp;
+    fp = fopen(file_name, "r");
+    if (fp == NULL  || !check_digit(file_name))
+    {
         printf(">> WARNING: File does not exsit. Please check folder!!\n");
         return false;
     }
-    printf(">> SUCCESS: File is existed in folder\n");
+    printf(">> Open file successful!!\n");
     fclose(fp);
     return true;
 }
 
-static void get_file_name(char* file_name){
+//check file exist in folder
+static bool check_file_exist(char* file_name, bool check_in_out){
+    bool check;
+    if(check_in_out){
+        check = is_exist_input_file(file_name);
+    }
+    else{
+        check = is_exist_out_file(file_name);
+    }
+    return check;
+}
+//enter name of file
+static void get_file_name(char* file_name, bool flag_check_input_output){
     __fpurge(stdin);
-    printf("Enter your file name: ");
+    if(flag_check_input_output){
+        printf("Enter your input file name: ");
+    }
+    else{
+        printf("Enter your output file name: ");
+    }
     fgets(file_name, MAX_FILE_NAME, stdin);
     file_name[strcspn(file_name, "\n")] = '\0';
 }
 
+//enter input file and check name is validate
 void input_file(char *file_name)
 {
     bool is_file_exist = false;
     do {
-        get_file_name(file_name);
-        is_alpha_num(file_name); // check input is alpha and number
-        is_file_exist = check_file_exist(file_name);
-        
-    } while (!isalnum(file_name[0]) || strlen(file_name) > 10 || !is_file_exist);
+        get_file_name(file_name, 1);
+        is_file_exist = check_file_exist(file_name, 1);
+    } while (!is_file_exist || strlen(file_name) > MAX_FILE_NAME);
 }
 
-void calculate_encrypt(FILE* input_file, FILE* output_file, bool flag, int shift){
-    int temp = shift;
+void encrypt(char *file_name, int shift, bool flag, char* output_file_name)
+{
     char ch;
+    int temp = shift;
+
+    FILE *input_file = fopen(file_name, "rb");
+    FILE *output_file = fopen(output_file_name, "w");
+
     if (!flag)
     {
         temp = 26 - shift;
     }
-    
+
     while ((ch = fgetc(input_file)) != EOF)
     {
         if (isalpha(ch))
@@ -98,40 +175,28 @@ void calculate_encrypt(FILE* input_file, FILE* output_file, bool flag, int shift
         fputc(ch, output_file);
         putchar(ch);
     }
-}
 
-void encrypt(char *file_name, int shift, bool flag)
-{
-    int temp = shift;
-
-    FILE *input_file = fopen(file_name, "rb");
-    FILE *output_file = fopen("output.txt", "a");
-
-    calculate_encrypt(input_file, output_file, 1, shift);
-    
     fclose(input_file);
     fclose(output_file);
 }
 // Function to decrypt the content of a file using Caesar cipher
-void decrypt(char *file_name, int shift)
+void decrypt(char *file_name, int shift, bool flag, char* output_file_name)
 {
-    encrypt(file_name, shift, 0);
+    encrypt(file_name, shift, 0, output_file_name);
 }
 
 void output_file(char* output_file_name)
 {
     bool is_file_exist = false;
     do {
-        get_file_name(output_file_name);
-        is_alpha_num(output_file_name); // check input is alpha and number
-        //is_file_exist = check_file_exist(file_name);
+        get_file_name(output_file_name, 0);
+        is_file_exist = is_over_range(output_file_name); 
         
-    } while (!isalnum(output_file_name[0]) || strlen(output_file_name) > 10);
+    } while (!is_file_exist || strlen(output_file_name) > MAX_FILE_NAME);
 }
 
-void change_new_output_file(char* output_file_name, int shift)
+void change_output_file(char* output_file_name)
 {
     output_file(output_file_name);
-    FILE *new_output_file = fopen(output_file_name, "a");
     
 }
