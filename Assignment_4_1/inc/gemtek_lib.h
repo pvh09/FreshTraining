120507239164d41854490d9e92acfd0af9a1568b
pvh09 <huyhoa09012001@gmail.com>
2023-09-05 16:29:23 +0700

    update code Ass 4 P1

--- Assignment_4_1/inc/gemtek_lib.h
+++ Assignment_4_1/inc/gemtek_lib.h
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <ctype.h>
-#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <stdio_ext.h>
+void get_input_integer(int* integer_number);
 
-void get_input_integer(int* integer_number );
+void print_square_cube_integer(int integer_number);
 
-void print_square_cube_integer(int integer_number );
+double calculate_square_integer(int integer_number);
 
-int calculate_square_integer(int integer_number );
-
-int calculate_cube_integer(int integer_number );
+double calculate_cube_integer(int integer_number);
 
 #endif
--- Assignment_4_1/src/gemtek_mathlib.c
+++ Assignment_4_1/src/gemtek_mathlib.c
@@ -1,43 +1,61 @@
 #include "gemtek_lib.h"
 
-static double validate_num_input()
+static int validate_num_input()
 {
     char input_string[20] = {0};
-    double input = -1;
+    long input = -1;
     char *p_end;
+
     while (1)
     {
+        __fpurge(stdin);
         scanf("%19s", input_string);
-            long long intPart = strtoll(input_string, &p_end, 10);
-            if (p_end != input_string && *p_end == '\0')
+
+        // Convert the input string to a long integer
+        input = strtol(input_string, &p_end, 10);
+
+        // Check for conversion errors
+        if (p_end != input_string && *p_end == '\0')
+        {
+            // Check if the input is within the range of INT_MIN and INT_MAX
+            if (input >= INT_MIN && input <= INT_MAX)
             {
-                input = intPart;
-                return input;
+                return (int)input; // Convert and return as an int
             }
-        printf("\nWARNING: ");
-        printf(">> Only input is integer. Please enter again: ");
+            else
+            {
+                printf("\nWARNING: Input out of range. Please enter again: ");
+            }
+        }
+        else
+        {
+            printf("\nWARNING: Only input integer is allowed. Please enter again: ");
+        }
     }
-    return input;
 }
 
-void get_input_integer(int* integer_number ){
+void get_input_integer(int *integer_number)
+{
     printf("Enter a integer number: ");
     *integer_number = validate_num_input();
 }
 
-int calculate_square_integer(int integer_number ){
-    int square = 0;
-    square =  integer_number * integer_number;
+double calculate_square_integer(int integer_number)
+{
+    double square = 0.0f;
+    square = (long long)integer_number * integer_number;
     return square;
 }
 
-int calculate_cube_integer(int integer_number ){
-    int cube = 0;
-    cube = integer_number * integer_number * integer_number;
+double calculate_cube_integer(int integer_number)
+{
+    double cube = 0.0f;
+    cube = pow(integer_number, 3);
     return cube;
 }
 
-void print_square_cube_integer(int integer_number ){
-    printf("Square of %d is %d\n",integer_number, calculate_square_integer(integer_number));
-    printf("Cube of %d is %d\n",integer_number, calculate_cube_integer(integer_number));
+void print_square_cube_integer(int integer_number)
+{
+    printf("Square is %0.2f\n", calculate_square_integer(integer_number));
+    printf("Cube is %0.2f\n", calculate_cube_integer(integer_number));
 }
