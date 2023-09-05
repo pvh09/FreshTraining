c037d18434c05a27b3385fb97fa9a60fbf0583cc
pvh09 <huyhoa09012001@gmail.com>
2023-09-05 16:24:36 +0700

    update code Ass 4 P2

--- Assignment_4_2/inc/gemtek_lib.h
+++ Assignment_4_2/inc/gemtek_lib.h
@@ -5,14 +5,16 @@
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
+#include <stdio_ext.h>
 
 void get_input_integer(int* integer_number);
 
 void print_square_cube_integer(int integer_number);
 
-int calculate_square_integer(int integer_number);
+double calculate_square_integer(int integer_number);
 
-int calculate_cube_integer(int integer_number);
+double calculate_cube_integer(int integer_number);
 
 int is_fibonacci(int integer_number);
 
--- Assignment_4_2/src/gemtek_mathlib.c
+++ Assignment_4_2/src/gemtek_mathlib.c
@@ -1,52 +1,74 @@
 #include "gemtek_lib.h"
 
-static double validate_num_input()
+int validate_num_input()
 {
     char input_string[20] = {0};
-    double input = -1;
+    long input = -1;
     char *p_end;
+
     while (1)
     {
+        __fpurge(stdin);
         scanf("%19s", input_string);
-            long long intPart = strtoll(input_string, &p_end, 10);
-            if (p_end != input_string && *p_end == '\0')
+
+        // Convert the input string to a long integer
+        input = strtol(input_string, &p_end, 10);
+
+        // Check for conversion errors
+        if (p_end != input_string && *p_end == '\0')
+        {
+            // Check if the input is within the range of INT_MIN and INT_MAX
+            if (input >= INT_MIN && input <= INT_MAX)
             {
-                input = intPart;
-                return input;
+                return (int)input; // Convert and return as an int
             }
-        printf("\nWARNING: ");
-        printf(">> Only input is integer. Please enter again: ");
+            else
+            {
+                printf("\nWARNING: Input out of range. Please enter again: ");
+            }
+        }
+        else
+        {
+            printf("\nWARNING: Only input integer is allowed. Please enter again: ");
+        }
     }
-    return input;
 }
 
-void get_input_integer(int* integer_number ){
+void get_input_integer(int *integer_number)
+{
     printf("Enter a integer number: ");
     *integer_number = validate_num_input();
 }
 
-int calculate_square_integer(int integer_number ){
-    int square = 0;
-    square =  integer_number * integer_number;
+double calculate_square_integer(int integer_number)
+{
+    double square = 0.0f;
+    //square = pow(integer_number, 2);
+    square = (long long)integer_number * integer_number;
     return square;
 }
 
-int calculate_cube_integer(int integer_number ){
-    int cube = 0;
-    cube = integer_number * integer_number * integer_number;
+double calculate_cube_integer(int integer_number)
+{
+    double cube = 0.0f;
+    cube = pow(integer_number, 3);
+    //cube = (double)(integer_number * (long long)integer_number * integer_number);
     return cube;
 }
 
-void print_square_cube_integer(int integer_number ){
-    printf("Square of %d is %d\n",integer_number, calculate_square_integer(integer_number));
-    printf("Cube of %d is %d\n",integer_number, calculate_cube_integer(integer_number));
+void print_square_cube_integer(int integer_number)
+{
+    printf("Square is %0.2f\n", calculate_square_integer(integer_number));
+    printf("Cube is %0.2f\n", calculate_cube_integer(integer_number));
 }
 
-int is_perfect_square(int x) {
-    int s = sqrt(x);
+int is_perfect_square(double x)
+{
+    double s = round(sqrt(x));
     return s * s == x;
 }
 
-int is_fibonacci(int integer_number) {
-    return is_perfect_square(5 * integer_number * integer_number + 4) || is_perfect_square(5 * integer_number * integer_number - 4);
+int is_fibonacci(int integer_number)
+{
+    return is_perfect_square((double)5 * integer_number * integer_number + 4) || is_perfect_square(5 * integer_number * integer_number - 4);
 }
